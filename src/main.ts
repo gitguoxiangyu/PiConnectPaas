86b2787c6eba4eb7fe57a91c0d3e4a20914c1f9c
Xiang Guo <xiang.guo@dolphindb.com>
2023-11-13 20:23:48 +0800

    style: 引入全局样式文件，去除默认样式

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import router from '@/router/index'
+import './style.css'
 
 const app = createApp(App)
 
