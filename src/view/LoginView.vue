ce4132e731a54dd905a53980efd0ab68ed5f24b7
xinger <1036710499@qq.com>
2023-11-17 18:40:17 +0800

    to:登陆界面
    微信登录的二维码接口好像有点问题，收不到二维码 暂时搁置

--- src/view/LoginView.vue
+++ src/view/LoginView.vue
@@ -1,227 +1,288 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { type FcResponse, get, post } from '@/utils/index'
-import { ElMessage } from 'element-plus'
-import type { FormRules } from 'element-plus'
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { type FcResponse, get, post } from "@/utils/index";
+import { ElMessage } from "element-plus";
+import type { FormRules } from "element-plus";
 
-const loginIng = ref(0)
-// 电话denglu
+const loginIng = ref(0);
+const router = useRouter();
+// 电话登录
+const subPhonenumBtn = ref();
+const phoneNumFormRef = ref();
 interface PhoneNumForm {
-  phoneNumber: number
-  code?: string
-  tempKey?: string
-  data?: string
+  phoneNumber?: number|null;
+  code?: string;
+  tempKey?: string;
+  data?: string;
 }
 const phoneNumForm = ref<PhoneNumForm>({
-  phoneNumber: 0,
-  code: '',
-  tempKey: ''
-})
+  phoneNumber: null,
+  code: "",
+  tempKey: "",
+});
 const phoneNumRules = ref<FormRules<PhoneNumForm>>({
-  phoneNumber: [{ required: true, message: '请输入手机号码', trigger: 'blur' }]
-})
+  phoneNumber: [
+    { required: true, message: "请输入手机号", trigger: "blur" },
+    {
+      pattern: /^[1-9]\d{10}$/,
+      message: "请输入正确的手机号码",
+      trigger: "blur",
+    },
+  ],
+});
 const subPhonenum = async () => {
+  await phoneNumFormRef.value.validate();
   const phoneNumObject = {
-    phoneNumber: phoneNumForm.value.phoneNumber
-  }
-
-  const [err, data] = await post<string>('/user/sendCodeToPhone', phoneNumObject)
-  if (err === null) {
-    ElMessage.error('网络繁忙')
-    console.log(err)
+    phoneNumber: phoneNumForm.value.phoneNumber,
+  };
+  const [err, data] = await post<string>(
+    "/user/sendCodeToPhone",
+    phoneNumObject
+  );
+  if (err !== null) {
+    console.log(err);
   } else {
-    console.log(data?.data)
-    phoneNumForm.value.tempKey = data?.data
-  }
-}
-const submitPhoneNum = async () => {
-  console.log(phoneNumForm.value)
+    console.log(subPhonenumBtn.value);
 
-  const [err, data] = await post<PhoneNumForm>('/user/verifyCode', phoneNumForm.value)
-  if (err)
-    ElMessage.error('服务器繁忙')
-  else
-    console.log(data)
-}
+    phoneNumForm.value.tempKey = data?.data;
+    let timeout = 10;
+    const timeoutCtrl = setInterval(() => {
+      if (timeout == 1) {
+        clearInterval(timeoutCtrl);
+      }
+      timeout--;
+    }, 1000);
+  }
+};
+const submitPhoneNum = async () => {
+  const [_, data] = await post<PhoneNumForm>(
+    "/user/verifyCode",
+    phoneNumForm.value
+  );
+  if (data?.code===403) {
+    ElMessage.error("验证码不匹配！");
+  } else {
+    console.log(data);
+    ElMessage("登录成功");
+    router.push('/')
+  }
+};
 // 微信登录相关
-const wechatloginable = ref(false)
-const QRcodeready = ref(false)
+const wechatloginable = ref(false);
+const QRcodeready = ref(false);
 interface wechatQRCode {
-  qrUrl?: string
-  tempUserId?: string
+  qrUrl?: string;
+  tempUserId?: string;
 }
 const wechatLoginInfo = ref<wechatQRCode>({
-  qrUrl: '',
-  tempUserId: ''
-})
+  qrUrl: "",
+  tempUserId: "",
+});
 const getQRcod = async (): Promise<FcResponse<wechatQRCode> | undefined> => {
-  try {
-    const [err, data] = await get<wechatQRCode>('/wxUser/wxQr')
-    if (err) {
-      console.error(err)
-      return undefined
-    } else return data
-  } catch (error) {
-    console.error(error)
-    return undefined
+  const [err, data] = await get<wechatQRCode>("/wxUser/wxQr");
+  if (err === null) {
+    console.error(err);
+    ElMessage.error("暂时无法获取！");
+    return undefined;
+  } else {
+    console.log(data);
+    return data;
   }
-}
+};
+
 const getResponse = async (): Promise<void> => {
-  // const lock = setInterval(async (): Promise<void> => {
-  //   const [err, data] = await get(
-  //     `/wxUser/isLogin?tempUserId=${wechatLoginInfo.value.tempUserId}`
-  //   )
-  // }, 1000)
-  // setTimeout(() => {
-  //   clearInterval(lock)
-  // }, 30000)
-  const [err, data] = await get(
-      `/wxUser/isLogin?tempUserId=${wechatLoginInfo.value.tempUserId}`
-  )
-  
-  setTimeout(async () => {
-    await getResponse()
-    return void
-  }, 1000)
-}
+  //   // const lock = setInterval(async (): Promise<void> => {
+  //   //   const [err, data] = await get(
+  //   //     `/wxUser/isLogin?tempUserId=${wechatLoginInfo.value.tempUserId}`
+  //   //   )
+  //   // }, 1000)
+  //   // setTimeout(() => {
+  //   //   clearInterval(lock)
+  //   // }, 30000)
+  //   // const [err, data] = await get(
+  //   //     `/wxUser/isLogin?tempUserId=${wechatLoginInfo.value.tempUserId}`
+  //   // )
+  //   // setTimeout(async () => {
+  //   //   await getResponse()
+  //   //   return void
+  //   // }, 1000)
+};
 const wechatlogin = async (): Promise<void> => {
-  wechatloginable.value = true
-  try {
-    const data = await getQRcod()
-    wechatLoginInfo.value.qrUrl = data?.data.qrUrl
-    wechatLoginInfo.value.tempUserId = data?.data.tempUserId
-    setTimeout(() => {
-      QRcodeready.value = true
-      getResponse()
-    }, 1000)
-  } catch (error) {
-    console.error(error)
-  }
-  console.log(wechatLoginInfo.value)
-}
+  wechatloginable.value = true;
+  const data = await getQRcod();
+  wechatLoginInfo.value.qrUrl = data?.data.qrUrl;
+  wechatLoginInfo.value.tempUserId = data?.data.tempUserId;
+  setTimeout(() => {
+    QRcodeready.value = true;
+    getResponse();
+  }, 1000);
+
+  console.log(wechatLoginInfo.value);
+};
 // 定义登录表单以及规则
+const ruleFormRef = ref();
 interface LoginForm {
-  username?: string
-  password?: string
-  repassword?: string
-  pic?: string
+  username?: string;
+  password?: string;
+  repassword?: string;
+  pic?: string;
 }
 const loginForm = ref<LoginForm>({
-  username: '',
-  password: '',
-  repassword: '',
-  pic: ''
-})
-const confirmpwd = (rule: any, value: any, callback: any): void => {
-  if (value === '') callback(new Error('请再次输入密码'))
-  else if (value !== loginForm.value.password)
-    callback(new Error('两次输入不匹配'))
-  else console.log(rule)
-}
+  username: "",
+  password: "",
+  repassword: "",
+  pic: "",
+});
 const loginRules = ref<FormRules<LoginForm>>({
-  username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+  username: [{ required: true, message: "请输入用户名", trigger: "blur" }],
   password: [
-    { required: true, message: '请输入密码', trigger: 'blur' },
-    { min: 3, max: 5, message: '密码位数', trigger: 'blur' }
+    { required: true, message: "请输入密码", trigger: "blur" },
+    {
+      pattern: /^\S{6,12}$/,
+      message: "密码必须为6-12位",
+      trigger: "blur",
+    },
   ],
   repassword: [
-    { validator: confirmpwd, trigger: 'blur' },
-    { required: true, message: '请输入密码', trigger: 'blur' }
-  ]
-})
+    { required: true, message: "请输入密码", trigger: "blur" },
+    {
+      validator: (_, value, callback) => {
+        // 判断 value 和 当前 form 中收集的 password 是否一致
+        if (value !== loginForm.value.password) {
+          callback(new Error("两次输入密码不一致"));
+        } else {
+          callback(); // 就算校验成功，也需要callback
+        }
+      },
+      trigger: "blur",
+    },
+  ],
+});
+const loginSubmit = async () => {
+  const picForm = ref({
+    uuid: pic.value.uuid,
+    codeOfUser: loginForm.value.pic,
+  });
+  const loginSubForm = ref({
+    userName: loginForm.value.username,
+    password: loginForm.value.password,
+  });
+  const [err, _] = await post("/captchaImage", picForm.value);
+  if (err === null) {
+    await ruleFormRef.value.validate();
+    const [_, data] = await post("/user/login", loginSubForm.value);
+    if (data?.code === 400) {
+      ElMessage.error("该手机号已经存在用户！");
+    } else {
+      console.log(data);
+      ElMessage("登录成功");
+      loginForm.value = {
+        username: "",
+        password: "",
+        repassword: "",
+        pic: "",
+      };
+      router.push("/");
+    }
+  }
+};
 // 定义注册表单及其相应规则
+const signinFormRef = ref();
 interface SigninForm {
-  phoneNumber: string
-  password: string
-  nickName: string
-  gender: '女' | '男'
-  age: number
+  phoneNumber: string;
+  password: string;
+  nickName: string;
+  gender: "女" | "男";
+  age: number;
 }
 const signinForm = ref<SigninForm>({
-  phoneNumber: '',
-  password: '',
-  nickName: '',
-  gender: '男',
-  age: 0
-})
+  phoneNumber: "",
+  password: "",
+  nickName: "",
+  gender: "男",
+  age: 0,
+});
 const signinRules = ref<FormRules<SigninForm>>({
   phoneNumber: [
-    { required: true, message: '请输入手机号', trigger: 'blur' },
-    { min: 11, max: 11, message: '手机号位数', trigger: 'blur' }
+    { required: true, message: "请输入手机号", trigger: "blur" },
+    {
+      pattern: /^[1-9]\d{10}$/,
+      message: "请输入正确的手机号码",
+      trigger: "blur",
+    },
   ],
   password: [
-    { required: true, message: '请输入密码', trigger: 'blur' },
-    { min: 4, max: 10, message: '密码为4-10位', trigger: 'blur' }
+    { required: true, message: "请输入密码", trigger: "blur" },
+    {
+      pattern: /^\S{6,12}$/,
+      message: "密码必须为6-12位",
+      trigger: "blur",
+    },
   ],
-  nickName: [{ required: true, message: '请输入昵称', trigger: 'blur' }]
-})
+  nickName: [{ required: true, message: "请输入昵称", trigger: "blur" }],
+});
 interface signinReturn {
-  userId: number
-  userName: string
-  nickName: string
-  gender: '男' | '女'
-  phoneNumber: string
-  avatarUrl: null
-  token: string
-  age: number
-  registerDays: null
+  userId: number;
+  userName: string;
+  nickName: string;
+  gender: "男" | "女";
+  phoneNumber: string;
+  avatarUrl: null;
+  token: string;
+  age: number;
+  registerDays: null;
 }
 const signinSubmit = async (): Promise<void> => {
-  try {
-    const [err, data] = await post<signinReturn>(
-      '/user/register',
-      signinForm.value
-    )
-    if (err) console.error(err)
-    else {
-      console.log(data)
-      ElMessage('创建成功')
-      loginIng.value = 0
-    }
-  } catch (error) {
-    console.error(error)
+  await signinFormRef.value.validate();
+  const [_, data] = await post<signinReturn>(
+    "/user/register",
+    signinForm.value
+  );
+  signinForm.value = {
+    phoneNumber: "",
+    password: "",
+    nickName: "",
+    gender: "男",
+    age: 0,
+  };
+  if (data?.code === 409) {
+    ElMessage.error("该手机号已被注册！");
+  } else {
+    ElMessage("注册成功");
+    loginIng.value = 0;
   }
-}
+};
 // 验证码
 const pic = ref<picIfo>({
-  img: '',
-  uuid: ''
-})
+  img: "",
+  uuid: "",
+});
 interface picIfo {
-  img?: string
-  captchaEnabled?: boolean
-  uuid?: string
+  img?: string;
+  captchaEnabled?: boolean;
+  uuid?: string;
 }
 const getNumPiC = async (): Promise<void> => {
-  try {
-    const [err, data] = await get<picIfo>('/captchaImage')
-    if (err) console.error(err)
-    else {
-      pic.value.img = data?.data.img
-      pic.value.uuid = data?.data.uuid
-      console.log(pic.value.img)
-    }
-  } catch (error) {
-    console.error(error)
+  const [err, data] = await get<picIfo>("/captchaImage");
+  if (err !== null) console.error(err);
+  else {
+    pic.value.img = data?.data.img;
+    pic.value.uuid = data?.data.uuid;
   }
-}
-// const  = async () => {
-//   const [err, data] = await get("/captchaImage");
-//   console.log(data);
-// };
+};
 onMounted(() => {
-  void getNumPiC()
-})
+  void getNumPiC();
+});
 </script>
-
 <template>
   <div class="loginPage">
     <el-row class="loginview">
-      <el-col :span="12" class="bg"></el-col>
+      <el-col :span="12" class="bg" />
       <el-col :span="12" class="formView">
         <h3>PiConnect</h3>
         <el-form
-          v-if="loginIng==0"
+          v-if="loginIng == 0"
           ref="ruleFormRef"
           :model="loginForm"
           :rules="loginRules"
@@ -235,22 +296,16 @@ onMounted(() => {
               placeholder="请输入用户名："
             />
           </el-form-item>
-          <el-form-item
-            label="密码"
-            prop="password"
-            style="margin-bottom: 12px"
-          >
+          <el-form-item label="密码" prop="password">
             <el-input
               v-model="loginForm.password"
               placeholder="请输入密码："
               type="password"
             />
-            <el-button link style="color: #999; font-size: 0.8em"
-              >忘记密码？</el-button
-            >
           </el-form-item>
-          <el-form-item label="确认密码" prop="repassword" type="password">
+          <el-form-item label="确认密码" prop="repassword">
             <el-input
+              type="password"
               v-model="loginForm.repassword"
               placeholder="请确认密码："
             />
@@ -261,21 +316,25 @@ onMounted(() => {
               placeholder=""
               style="max-width: 100px"
             />
-            <img :src="`data:image/png;base64,${pic.img}`" @click="getNumPiC">
+            <img :src="`data:image/png;base64,${pic.img}`" @click="getNumPiC" />
           </el-form-item>
-          <div style="display: flex; justify-content: center; margin-top: 15%">
-            <el-button style="width: 30%" @click="loginIng=1">注册</el-button>
-            <el-button style="width: 30%">登录</el-button>
+          <div style="display: flex; justify-content: center; margin-top: 10%">
+            <el-button style="width: 30%" @click="loginIng = 1">
+              注册
+            </el-button>
+            <el-button style="width: 30%" @click="loginSubmit">
+              登录
+            </el-button>
           </div>
         </el-form>
         <el-form
-          v-if="loginIng==1"
+          v-if="loginIng == 1"
           ref="signinFormRef"
           :model="signinForm"
           :rules="signinRules"
           label-width="80px"
-          style="margin-top: 10px"
           status-icon
+          class="loginMenu"
         >
           <el-form-item label="手机号码" prop="phoneNumber">
             <el-input
@@ -302,30 +361,50 @@ onMounted(() => {
             <el-button
               style="margin-left: -10px; width: 40%"
               @click="loginIng = 0"
-              >去登录</el-button
             >
-            <el-button @click="signinSubmit" style="width: 45%"
-              >立即注册</el-button
-            >
+              去登录
+            </el-button>
+            <el-button style="width: 45%" @click="signinSubmit">
+              立即注册
+            </el-button>
           </el-form-item>
         </el-form>
-        <el-form v-if="loginIng==2"
-          ref="signinFormRef"
+        <el-form
+          v-show="loginIng == 2"
+          ref="phoneNumFormRef"
           :model="phoneNumForm"
           :rules="phoneNumRules"
           label-width="80px"
-          style="margin-top: 10px"
+          style="margin-top: 10%"
           status-icon
-          >
+          class="phoneMenu"
+        >
           <el-form-item label="手机号码" prop="phoneNumber">
-            <el-input v-model.number="phoneNumForm.phoneNumber" placeholder="请输入手机号码" />
+            <el-input
+              v-model.number="phoneNumForm.phoneNumber"
+              placeholder="请输入手机号码"
+            />
           </el-form-item>
           <el-form-item label="验证码" prop="code">
-            <el-input v-model="phoneNumForm.code" placeholder="请输入验证码" />
-            <el-button @click="subPhonenum">点击发送</el-button>
+            <el-input
+              v-model="phoneNumForm.code"
+              style="max-width: 85px; margin-right: 5px"
+            />
+            <el-button @click="subPhonenum" ref="subPhonenumBtn">
+              点击发送
+            </el-button>
           </el-form-item>
-          <el-button>返回</el-button>
-          <el-button @click="submitPhoneNum">登录</el-button>
+          <el-form-item>
+            <el-button
+              style="margin-left: -10px; width: 40%"
+              @click="loginIng = 0"
+            >
+              返回
+            </el-button>
+            <el-button style="width: 45%" @click="submitPhoneNum">
+              登录
+            </el-button>
+          </el-form-item>
         </el-form>
         <div class="changeBox">
           <div
@@ -343,9 +422,10 @@ onMounted(() => {
             alt=""
             style="width: 20px; height: 20px; vertical-align: middle"
           />
-          <el-button link style="height: 100%" @click="loginIng=2">手机登录</el-button
+          <el-button link style="height: 100%" @click="loginIng = 2">
+            手机登录 </el-button
           ><span>or</span
-          ><el-button link @click="wechatlogin">扫码登录</el-button>
+          ><el-button link @click="wechatlogin"> 扫码登录 </el-button>
           <img
             src="@/assets/wechat.png"
             alt=""
@@ -361,7 +441,6 @@ onMounted(() => {
     </el-row>
   </div>
   <el-dialog v-model="wechatloginable" title="扫码登陆" width="350px" draggable>
-    <div></div>
     <template #footer>
       <img
         v-if="QRcodeready"
@@ -392,6 +471,7 @@ onMounted(() => {
   justify-content: center;
   align-items: center;
   .loginview {
+    overflow: hidden;
     background-color: #fff;
     width: 45%;
     height: 45%;
@@ -419,13 +499,26 @@ onMounted(() => {
       }
       .changeBox {
         position: absolute;
-        bottom: 2%;
+        top: 88%;
+      }
+      .el-form-item {
+        margin-bottom: 10%;
+      }
+      @media screen and (max-height: 800px) {
+        .el-form-item {
+          margin-bottom: 4%;
+        }
       }
       .el-input {
         width: 90%;
       }
       .el-input-number {
         width: 90%;
+      }
+      .phoneMenu{
+        .el-form-item {
+          margin-bottom: 10%;
+        }
       }
     }
   }
