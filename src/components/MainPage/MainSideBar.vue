24fe60c98a4c95a39d1df5a80779a99a3692ea80
jobgege <2638874938@qq.com>
2023-11-21 20:02:13 +0800

    feat：菜单折叠，组件化代码

---
+++ src/components/MainPage/MainSideBar.vue
@@ -0,0 +1,95 @@
+<template>
+  <div
+    class="toggle-button"
+    @click="toggleCollapse"
+  >
+    |||
+  </div>
+  <el-menu
+    background-color="#333744"
+    class="el-menu-vertical-demo"
+    active-text-color="#ffd04b"
+    text-color="#fff"
+    router
+    default-active="2"
+    :collapse="isCollapse"
+    @open="handleOpen"
+    @close="handleClose"
+  >
+    <el-sub-menu index="1">
+      <template #title>
+        <el-icon>
+          <Setting />
+        </el-icon>
+        <span> 系统界面
+        </span>
+      </template>
+      <el-menu-item-group title="Group One">
+        <el-menu-item index="/first">
+          界面一
+        </el-menu-item>
+        <el-menu-item index="/second">
+          界面二
+        </el-menu-item>
+      </el-menu-item-group>
+      <el-menu-item-group title="Group Two">
+        <el-menu-item index="/third">
+          界面三
+        </el-menu-item>
+      </el-menu-item-group>
+    </el-sub-menu>
+    <el-menu-item index="/othermes">
+      <el-icon>
+        <Location />
+      </el-icon>
+      <span>  其它信息
+      </span>
+    </el-menu-item>
+    <el-menu-item
+      index="/more"
+      disabled
+    >
+      <el-icon>
+        <Document />
+      </el-icon>
+      <span>更多介绍
+      </span>
+    </el-menu-item>
+  </el-menu>
+</template>
+<script lang="ts" setup>
+import { ref, defineEmits } from 'vue'
+import {
+  Document,
+  Location,
+  Setting
+} from '@element-plus/icons-vue'
+
+const isCollapse = ref(false)
+const handleOpen = (key: string, keyPath: string[]): void => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]): void => {
+  console.log(key, keyPath)
+}
+const toggleCollapse = (): void => {
+  isCollapse.value = !isCollapse.value // 切换 isCollapse 的值，实现折叠和展开的切换
+  emit('fn')
+}
+
+const emit = defineEmits<(event: 'fn') => void>()
+</script>
+<style lang="scss" scoped>
+.toggle-button{
+  background-color: #4A5064;
+  font-size: 10px;
+  color:#fff;
+  text-align: center;
+  letter-spacing:0.2em;
+  line-height:24px;
+  cursor: pointer;
+}
+.el-menu{
+ border:0!important;
+}
+</style>
--- src/view/MainPage.vue
+++ src/view/MainPage.vue
@@ -1,67 +1,15 @@
 <template>
   <el-container style="height: 100vh">
     <el-header>
-      <div class="head">
-        <div>
-          <span>可视化管理系统</span>
-        </div>
-        <el-menu
-          mode="horizontal"
-          background-color="#373d41"
-          text-color="#fff"
-          router
-        >
-          <el-button
-            type="info"
-            @click="logout"
-          >
-            退出
-          </el-button>
-          <el-menu-item index="/direction">
-            查看方向
-          </el-menu-item>
-        </el-menu>
-      </div>
+      <!-- 头部组件 -->
+      <MainHeader />
     </el-header>
     <el-container>
-      <el-aside>
-        <el-col :span="24">
-          <el-menu
-            background-color="#333744"
-            class="el-menu-vertical-demo"
-            active-text-color="#ffd04b"
-            text-color="#fff"
-            router
-          >
-            <el-sub-menu index="1">
-              <template #title>
-                <span>系统界面</span>
-              </template>
-              <el-menu-item-group title="Group One">
-                <el-menu-item index="/first">
-                  界面一
-                </el-menu-item>
-                <el-menu-item index="/second">
-                  界面二
-                </el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="Group Two">
-                <el-menu-item index="/third">
-                  界面三
-                </el-menu-item>
-              </el-menu-item-group>
-            </el-sub-menu>
-            <el-menu-item index="/othermes">
-              <span>其它信息</span>
-            </el-menu-item>
-            <el-menu-item
-              index="/more"
-              disabled
-            >
-              <span>更多介绍</span>
-            </el-menu-item>
-          </el-menu>
-        </el-col>
+      <el-aside :width="collapse ? '65px' : '220px'">
+        <MainSideBar
+          :is-collapse="collapse"
+          @fn="chanNum"
+        />
       </el-aside>
       <el-main>
         <router-view />
@@ -70,28 +18,15 @@
   </el-container>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import { useRouter } from 'vue-router'
-import { ElMessage } from 'element-plus'
+<script lang="ts" setup>
+import MainHeader from '../components/MainPage/MainHeader.vue'
+import MainSideBar from '../components/MainPage/MainSideBar.vue'
+import { ref } from 'vue'
+const collapse = ref(false)
+const chanNum = (): void => {
+  collapse.value = !collapse.value
+}
 
-export default defineComponent({
-  name: 'MainPage',
-  setup () {
-    const router = useRouter()
-    const logout = async (): Promise<void> => {
-      ElMessage({
-        showClose: true,
-        type: 'success',
-        message: '您已退出登录!'
-      })
-      await router.push('/login')
-    }
-    return {
-      logout
-    }
-  }
-})
 </script>
 
 <style lang="scss" scoped>
@@ -142,13 +77,13 @@ body, html {
 
 .el-aside {
   background-color: #333744;
-  width: 200px;
+  transition: width 0.2s;
   border-bottom-left-radius: 10px;  /* 添加圆角属性 */
+  border:0!important;
 
-  .el-menu {
-    border-right: none;
-    border: 0 !important;
-  }
+}
+.el-menu{
+  border: 0 !important;
 }
 
 .el-main {
@@ -163,10 +98,6 @@ body, html {
   text-align: center;
   letter-spacing: 0.2em;
   cursor: pointer;
-}
-
-.el-menu {
-  height: 100%;
 }
 
 .router-link-active {
